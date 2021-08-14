7152c8992eceee64855d558fd9aeac298aaf040b
mhoobler <hoobler.michael@gmail.com>
2021-08-14 19:55:21 -0400

    GHP fixes

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ app.use("/js", express.static(__dirname + "/docs/js"));
 app.use("/css", express.static(__dirname + "/docs/css"));
 
 app.get("/", (req, res) => {
-  // Something to fix bug with browser-refresh
-  // This is also good for GHP
+  // bunch of stuff so this plays nicely with GHP
   const html = fs.readFileSync("./docs/index.html");
   const $ = cheerio.load(html);
+  const devSrcs = [...$("script")];
+  const devHrefs = [...$("link")];
+
+  for (let tag of devSrcs) {
+    const srcArr = $(tag).attr("src").split("/");
+    const newSrc = "/" + srcArr.slice(1, srcArr.length).join("/");
+    $(tag).attr("src", newSrc);
+  }
+
+  for (let tag of devHrefs) {
+    const hrefArr = $(tag).attr("href").split("/");
+    const newHref = "/" + hrefArr.slice(1, hrefArr.length).join("/");
+    $(tag).attr("href", newHref);
+  }
+
   $("body").append(
     `<script src="${process.env.BROWSER_REFRESH_URL}"></script>`
   );
